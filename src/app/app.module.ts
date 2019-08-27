2bb18192603b27a6659492d72f57f07579664f3e
Marcelo <marcelo.souza@dce.ufpb.br>
2019-08-27 13:02:56 -0300

    Tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ShowClienteComponent } from './cliente/show-cliente/show-cliente.compon
 import { ListClientesComponent } from './cliente/list-clientes/list-clientes.component';
 import { EditClienteComponent } from './cliente/edit-cliente/edit-cliente.component';
 import { NewClienteComponent } from './cliente/new-cliente/new-cliente.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { NewClienteComponent } from './cliente/new-cliente/new-cliente.component
     ListClientesComponent,
     EditClienteComponent,
     NewClienteComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
