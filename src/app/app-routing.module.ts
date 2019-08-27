a7872c62d75588ed84a9d3d34ddbfcb2c33cbf2a
Marcelo <marcelo.souza@dce.ufpb.br>
2019-08-27 15:47:26 -0300

    Alterando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import { ClienteComponent } from './cliente/cliente/cliente.component'
 import { ListClientesComponent } from './cliente/list-clientes/list-clientes.component';
 import { NewClienteComponent } from './cliente/new-cliente/new-cliente.component';
 import { ShowClienteComponent } from './cliente/show-cliente/show-cliente.component';
 import { EditClienteComponent } from './cliente/edit-cliente/edit-cliente.component';
 
 const routes: Routes = [
-  { path: 'clientes', component: ListClientesComponent },
-  { path: 'clientes/novo', component: NewClienteComponent },
-  { path: 'clientes/:id', component: ShowClienteComponent },
-  { path: 'clientes/:id/editar', component: EditClienteComponent }
+  { path: '', component: LoginComponent },
+
+  {
+    path: 'clientes', component: ClienteComponent, children: [
+      { path: 'lista', component: ListClientesComponent },
+      { path: 'novo', component: NewClienteComponent },
+      { path: ':id', component: ShowClienteComponent },
+      { path: 'editar/:id', component: EditClienteComponent }
+    ]
+  },
 ];
 
 @NgModule({
