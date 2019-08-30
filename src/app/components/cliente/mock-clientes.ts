a43ea118ad4e3dbdd63406374e617f45d8d1c373
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-08-29 22:12:02 -0300

    Update R5 baseado em T1.2

--- src/app/components/cliente/mock-clientes.ts
+++ src/app/components/cliente/mock-clientes.ts
@@ -1,5 +1,7 @@
 import {Cliente} from "./cliente";
 import { Endereco } from './endereco';
+import { Locador } from './locador';
+import { Locatario } from './locatario';
 /*
 'Gustavo Maciel', "1234567", true,
       "1234567890", "83999999999", 
@@ -8,9 +10,14 @@ import { Endereco } from './endereco';
           "rua", "centro", "1234", "12345678"
       ), "Executivo"
 */
-export const CLIENTES: Cliente[] = [
-    {id: 1, nome:'Gustavo Maciel', rg:'1234567',
-    tipoCadastroPessoa:true, cadastroPessoa:'123456890', telefone: '83999999999', 
-    email:'gustavo.maciel@dce.ufpb.br', profissao: 'Estudante', 
+export const LOCADORES: Locador[] = [
+    {id: 1, nome:'John Doe', rg:'1234567', cnpj: '05.526.392/0001-40', telefone: '83999999999', 
+    email:'john.doe@gmail.com', profissao: 'CEO', 
     endereco: new Endereco('rua','bairro','numero','cep'), tipoCliente:'Locador'}
+]
+
+export const LOCATARIOS: Locatario[] = [
+    {id: 2, nome:'Gustavo Maciel', rg:'1234567', cpf: '123.321.123-32', telefone: '83999999999', 
+    email:'gustavo.maciel@dce.ufpb.br', profissao: 'Estudante', 
+    endereco: new Endereco('rua','bairro','numero','cep'), tipoCliente:'Locatario'}
 ]
