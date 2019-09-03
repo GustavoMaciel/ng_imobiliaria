9e196cb34bc8d69d34fa40ad13e13c3538c762fb
Marcelo <marcelo.souza@dce.ufpb.br>
2019-09-03 09:23:22 -0300

    Tela de Show Cliente

--- src/app/components/cliente/list-clientes/list-clientes.component.ts
+++ src/app/components/cliente/list-clientes/list-clientes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Cliente } from '../cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-list-clientes',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-clientes.component.css']
 })
 export class ListClientesComponent implements OnInit {
+  public cliente: Cliente;
 
-  constructor() { }
-
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clienteService: ClienteService
+  ) { }
   ngOnInit() {
+    this.route.paramMap.pipe(
+      switchMap(
+        (params: ParamMap) => this.loadCliente(+params.get('id'))
+      ))
+      .subscribe(
+        (cliente: Cliente) => this.cliente = cliente
+      );
+  }
+
+  loadCliente(idCliente: number): Promise<Cliente> {
+    return new Promise(
+      (resolve) => resolve(this.clienteService.getById(idCliente))
+    )
+  }
+
+  back() {
+    this.router.navigate(['/clientes']);
+    this.clienteService.clearMessage();
+    return false;
+  }
+
+  edit() {
+    this.clienteService.clearMessage();
+    this.router.navigate(['/clientes', this.cliente.id, 'editar']);
+    return false;
   }
 
 }
